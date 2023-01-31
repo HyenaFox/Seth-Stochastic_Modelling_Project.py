53f51f4b405c86e399eb30c5dfc59ea22536eba1
blaze <sethhaypol@gmail.com>
2023-01-31 01:27:19 -0500

    backup

--- main.py
+++ main.py
@@ -3,17 +3,17 @@ from turtle import *
 import pandas as pd
 import csv
 
-# TODO: RUN A MILLION TIMES, ALSO ADD ANIMATION
+# FUTURE PLANS: add animation
 StartingTotal = 6500
-CustomChange1 = randrange(-1000, 1000)
-CustomChange2 = randrange(-1000, 1000)
-CustomChange3 = randrange(-1000, 1000)
-CustomChange4 = randrange(-1000, 1000)
-CustomChange5 = randrange(-1000, 1000)
-CustomChange6 = randrange(-1000, 1000)
-CustomChange7 = randrange(-1000, 1000)
-CustomChange8 = randrange(-1000, 1000)
-CustomChange9 = randrange(-1000, 1000)
+CustomChange1 = randrange(-800, 800)
+CustomChange2 = randrange(-800, 800)
+CustomChange3 = randrange(-800, 800)
+CustomChange4 = randrange(-800, 800)
+CustomChange5 = randrange(-800, 800)
+CustomChange6 = randrange(-800, 800)
+CustomChange7 = randrange(-800, 800)
+CustomChange8 = randrange(-800, 800)
+CustomChange9 = randrange(-800, 800)
 
 RealLifeChanges = [0, -650, 400, 700, -300, 350, -150, 250, -150, 850]
 
@@ -98,13 +98,14 @@ for i in range(2900):
     CorrectCarList.append(AddCar(0))
 
 
-# TODO: Comment out text "Reports", remake it with just numbers and then assign it to the dataframe to be added to the CSV
 def CorrectForNextExit(Exit, OnRamp):
     NotEqual = len(CorrectCarList) != SixToSevenTotal[Exit]
     CarsRemaining = len(CorrectCarList)
+    CarsInTotal = len(CorrectCarList)
     CarsGettingOffHere = []
     CarsGettingOnHere = []
     Reports = []
+    ListReports = []
     Colors = 0
     GotOffCount = "No cars exited the highway."
     GotOnCount = "No Cars entered the highway."
@@ -139,6 +140,8 @@ def CorrectForNextExit(Exit, OnRamp):
             CarsRemaining = len(CorrectCarList)
             Colors = [NumYellows, NumBlues, NumGreens, NumReds, NumPurples]
             GotOnCount = f"{NumberCarsGetOnHere} entered the highway."
+            NumberCarsGetOffHere = 0
+            ListReports = [Exit, Colors, NumberCarsGetOffHere, NumberCarsGetOnHere, CarsRemaining, CarsInTotal]
             Reports = (f"At Exit Number {Exit}, "
                        f"{NumberCarsGetOnHere} cars entered the highway. \n"
                        f"Of the cars that got on, there were "
@@ -179,30 +182,64 @@ def CorrectForNextExit(Exit, OnRamp):
                        f"and {NumPurples} purple cars. \n"
                        f"There are {CarsRemaining} cars remaining.")
             GotOffCount = f"{NumberCarsGetOffHere} cars exited the highway."
+            NumberCarsGetOnHere = 0
+            ListReports = [Exit, Colors, NumberCarsGetOffHere, NumberCarsGetOnHere, CarsRemaining, CarsInTotal]
         if len(Reports) == 0:
             Colors = ("You'll never see this. And it's not because I don't want you to... just that this can't occur."
-                      "You're in a dream, you know.")
+                      "You're in a dream, you know. They say the reverie of the mind's deep slumber reveals"
+                      "unimaginable insight into the heart's desires. What do you dream of...? What do I dream of...?"
+                      "I think we can all only seek to know.")
             Reports.append(["You are at the last stop, which is theoretically impossible, because this code will "
                             "never reach this. It is a mathematical impossibility. This should be impossible, "
                             "this line is just here so that it prints... SOMETHING instead of an error. You know that "
                             "feeling, don't you, dear reader?"])
-        return CorrectCarList, Reports, Colors, GotOnCount, GotOffCount, CarsRemaining
+            ListReports = "Ohh, unthinkable error. A scattered dream that feels more like a far-off memory. " \
+                          "A memory that feels more like a scattered dream. Also. Colors list."
+            ListReports += Colors
+        return CorrectCarList, Reports, ListReports
 
-ResultDataIterable = {
 
+FullData = {
+    'Exit': [],
+    'Yellows': [],
+    'Blues': [],
+    'Greens': [],
+    'Reds': [],
+    'Purples': [],
+    'Total Cars': [],
+    'Cars Getting On At This Exit': [],
+    'Cars Getting Off At This Exit': [],
+    'Cars Left': []
 }
-ResultData = {
-    'ColorReports': [],
-    'EnterReports': [],
-    ''
-    
-    'TextReports': []
-}
-for MyIterable in range(1, 10):
-    FCarList = CorrectForNextExit(MyIterable, 1)
-    ResultData['TextReports'].append(FCarList[1])
+TextReadouts = ""
+FullData['Set Number'] = []
+# FOR BOOKKEEPING:
+# ListReports = [Exit, Colors, GotOnCount, GotOffCount, CarsRemaining]
+# Colors = [NumYellows, NumBlues, NumGreens, NumReds, NumPurples]
+for y in range(2):
+    for MyIterable in range(1, 10):
+        SetNum = y + 1
+        FinalsCarList = CorrectForNextExit(MyIterable, 1)
+        TextReadouts += FinalsCarList[1]
 
-print(ResultData)
+        FullData['Exit'].append(FinalsCarList[2][0])
+        FullData['Yellows'].append(FinalsCarList[2][1][0])
+        FullData['Blues'].append(FinalsCarList[2][1][1])
+        FullData['Greens'].append(FinalsCarList[2][1][2])
+        FullData['Reds'].append(FinalsCarList[2][1][3])
+        FullData['Purples'].append(FinalsCarList[2][1][4])
+        FullData['Cars Getting On At This Exit'].append(FinalsCarList[2][3])
+        FullData['Cars Getting Off At This Exit'].append(FinalsCarList[2][2])
+        FullData['Total Cars'].append(FinalsCarList[2][5])
+        FullData['Cars Left'].append(FinalsCarList[2][4])
+        FullData['Set Number'].append(SetNum)
+
+
+# print(TextReadouts)
+df = pd.DataFrame(FullData)
+
+df.to_csv("C:/Users/blaze/OneDrive/Documents/Seth - Stochastic Modelling Project/Final Results.csv")
+
 
 
 # def ListCreation():
